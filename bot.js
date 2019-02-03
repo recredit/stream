17d0c3cc4ef36b7a7f45501dd69c43b3b833a814
recredit <46069475+recredit@users.noreply.github.com>
2019-02-03 13:58:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "$";
 var adminprefix = '#'
 
 
-const developers = ["ID","ID"]
+const developers = ["526035894071984132","515173684248444930"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
